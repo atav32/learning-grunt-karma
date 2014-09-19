3bb043f53fff961de6bd8a2622b39cc1f75571e4
Brian Zhang <tangerinewhite32@gmail.com>
2014-09-18 18:57:11 -0700

    refactored directive test

--- test/components/version-directive_test.js
+++ test/components/version-directive_test.js
@@ -1,17 +1,24 @@
 'use strict';
 
 describe('myApp.version module', function() {
+  var $compile;
+  var $rootScope;
+
+  // load the app module 
   beforeEach(module('myApp.version'));
 
-  describe('app-version directive', function() {
-    it('should print current version', function() {
-      module(function($provide) {
-        $provide.value('version', 'TEST_VER');
-      });
-      inject(function($compile, $rootScope) {
-        var element = $compile('<span app-version></span>')($rootScope);
-        expect(element.text()).toEqual('TEST_VER');
-      });
-    });
+  // initialize the directive
+  beforeEach(module(function($provide) {
+      $provide.value('version', 'TEST_VER');
+  }));
+
+  beforeEach(inject(function(_$compile_, _$rootScope_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
+
+  it('should show current version', function() {
+    var element = $compile('<span app-version></span>')($rootScope);
+    expect(element.text()).toEqual('TEST_VER');
   });
 });
